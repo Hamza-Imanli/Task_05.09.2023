31f78ef1e835fec862ef617764ba7951394c022f
Hamza-Imanli <hamzaimanli2@gmail.com>
2023-09-05 12:05:51 +0400

    Task3

---
+++ Task_05.09.2023/Task_05.09.2023/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_05._09._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir eded daxil edin: ");
+            int a= Convert.ToInt32(Console.ReadLine());
+
+            while (a % 3 == 0)
+            {
+                a/= 3;
+            }
+
+            bool b = a == 1;
+
+            Console.WriteLine(b);
+        }
+    }
+}
